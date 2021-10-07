b70e7300479fc7babd9ff6ec4cf17e93b74c70b8
Joshua Liang <joshualiang.xy@gmail.com>
2021-10-07 21:52:27 +0800

    Feature/config (#1)
    * Add pre-commit hooks
    
    * Update pre-commit hooks
    
    * Add config files
    
    * Update README
    
    * Fix mongoose dependencies

---
+++ server/src/config/mongoose.config.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  MongooseModuleOptions,
+  MongooseOptionsFactory,
+} from '@nestjs/mongoose';
+import { format } from 'url';
+
+export interface MongoDBConfig {
+  host: string;
+  protocol: string;
+  database: string;
+  username?: string;
+  password?: string;
+}
+
+interface Options {
+  uri: string;
+  dbName: string;
+  auth?: {
+    username: string;
+    password: string;
+  };
+}
+
+@Injectable()
+export default class MongooseConfigService implements MongooseOptionsFactory {
+  public constructor(private readonly configService: ConfigService) {}
+
+  public createMongooseOptions(): MongooseModuleOptions {
+    const {
+      host,
+      protocol,
+      username,
+      password,
+      database: dbName,
+    } = this.configService.get<MongoDBConfig>('mongodb');
+    const options: Options = {
+      uri: format({
+        protocol,
+        host,
+        slashes: true,
+      }),
+      dbName,
+    };
+
+    if (username) options.auth = { username, password };
+
+    return options;
+  }
+}
