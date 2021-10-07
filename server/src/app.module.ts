c7ca9ccd1b570475fbc90a1f53fda27c9039913c
joshualiangxy <joshualiang.xy@gmail.com>
2021-10-08 00:58:31 +0800

    Remove boilerplate files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import config from './config';
 import MongooseConfigService from './config/mongoose.config';
 import QuestionsModule from './questions/questions.module';
@@ -16,7 +14,5 @@ import QuestionsModule from './questions/questions.module';
     }),
     QuestionsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
