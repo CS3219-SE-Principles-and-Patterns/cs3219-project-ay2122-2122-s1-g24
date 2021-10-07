29a1476c26e41d30e56b2c08e53a5520b7f29817
joshualiangxy <joshualiang.xy@gmail.com>
2021-10-08 00:56:44 +0800

    Add unit tests for QuestionsRepository

--- server/src/questions/questions.repository.ts
+++ server/src/questions/questions.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { QuestionNotFoundError } from 'src/errors';
+import { QuestionNotFoundError } from '../errors';
 import { Difficulty } from './questions.const';
 import { Question, QuestionDocument } from './questions.schema';
 
