63baeb3ef80323cd952eb703a0e147151095ed14
Rishabh Paliwal <rishabh92k@gmail.com>
2021-11-01 20:57:05 +0800

    Add Rooms (#15)
    * init front end
    
    * Add button for homepage
    
    * Commit front end changes
    
    * no message
    
    * no message
    
    * add login page
    
    * implement Google Auth
    
    * Add logout to add
    
    * remove deadcode
    
    * Add code env in room
    
    * no message
    
    * add context auth
    
    * Fix navbar
    
    * Change link in navbar
    
    * add history to dropdown
    
    * Remove dupes
    
    * Fix bug on ligma
    
    * Add template for socket connection code
    
    * Socket.io doesnt connect
    
    * Fix gateway
    
    * Fix room and codeEnv socket emit
    
    * Reconnect socket
    
    * Add PartnerInfo
    
    * Add PartnerName
    
    * Fix partnerName
    
    * Fix CORS issue
    
    * Fix warnings
    
    Co-authored-by: Roger <E0406757@u.nus.edu>
    Co-authored-by: joshualiangxy <joshualiang.xy@gmail.com>

--- frontend/src/component/dropdown.js
+++ frontend/src/component/dropdown.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container'
-import { Link, Redirect } from 'react-router-dom'
 import style from './dropdown.css'
 import Dropdown from 'react-bootstrap/Dropdown'
-import { AuthProvider, useAuth } from  "../context/AuthContext";
+import {useAuth } from  "../context/AuthContext";
 import Button from 'react-bootstrap/Button'
 import io from "socket.io-client";
 import { useHistory } from 'react-router-dom';
@@ -13,20 +12,27 @@ let difficulty = "pick a difficulty"
 var DropDownMenu = () => {
     const history = useHistory();
     const { token } = useAuth();
-    const socket = io('http://localhost:8080/matchmaking', { transports: ['websocket'] });
+    const socket = io('ws://localhost:8080/matchmaking', { transports: ['websocket'] });
+
+    socket.on('assignRoom' , match => {
+        console.log(match);
+        history.push(`/room/${match}`);
+    });
+
     const matchMake = () => {
-        console.log('matchmaking');
         socket.emit('findMatch', {
-            "difficulty" : difficulty,
+            "difficulty" : difficulty, 
             "auth" : token
         }, (response) => {
-            console.log(response.status); 
+            if (response.err) {
+                alert("error");
+            }
         });
+        socket.on('assignRoom' , (match) => {
+            history.push("/room/" + match)
+        })
     };
 
-    socket.on('assignRoom', (match) => {
-        history.push(`room/${match["roomId"]}`)
-    });
 
     return (
         <Container fluid style = {{marginTop : "20px"}}>
@@ -38,9 +44,9 @@ var DropDownMenu = () => {
                     {difficulty}
                 </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item href="#/easy" onClick={()=> {difficulty = "Easy"}} >Easy</Dropdown.Item>
-                <Dropdown.Item href="#/medium" onClick={()=> {difficulty = "Medium"}}>Medium</Dropdown.Item>
-                <Dropdown.Item href="#/hard" onClick={()=> {difficulty = "Hard"}}>Hard</Dropdown.Item>
+                <Dropdown.Item href="#/easy" onClick={()=> {difficulty = "easy"}} >Easy</Dropdown.Item>
+                <Dropdown.Item href="#/medium" onClick={()=> {difficulty = "medium"}}>Medium</Dropdown.Item>
+                <Dropdown.Item href="#/hard" onClick={()=> {difficulty = "hard"}}>Hard</Dropdown.Item>
             </Dropdown.Menu>
             </Dropdown>
             <Button variant="primary" onClick={matchMake}>Find a match</Button>{' '}
--- frontend/src/component/room.js
+++ frontend/src/component/room.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuth } from  "../context/AuthContext";
+import io from "socket.io-client";
 import CodeEnv from './codeEnv';
 import Question from './question';
+import PartnerInfo from './partnerInfo';
 
-const DEFAULT_VALUE = '// Enter code here'
+const Room = (props) => {
+    const [ questionTitle, setQuestionTitle ] = useState();
+    const [ questionDesc, setQuestionDesc ] = useState();
+    const [ names, setNames ] = useState();
 
-const Room = () => {
+    const id = props.match.params.id;
+    const { token } =  useAuth();
+    const socket = io('ws://localhost:8080/rooms', { transports: ['websocket'] });
+
+    useEffect(() => {
+        socket.emit('joinRoom', {auth: token, room: id}, roomDetails => {
+            if(roomDetails === undefined) return;
+            setQuestionTitle(roomDetails.questionTitle);
+            setQuestionDesc(roomDetails.questionDesc);
+            setNames(roomDetails.users);
+        });
+
+        return function cleanup() {
+            socket.emit("endSession", {auth: token, room: id});
+        };
+    }, []);
+
     return (
         <div>
-            <Question/>
-            <CodeEnv/>
+            <PartnerInfo names = { names } />
+            <Question questionTitle = { questionTitle } questionDesc = { questionDesc } />
+            <CodeEnv id = { id } socket = { socket } />
         </div>
     )
 }
