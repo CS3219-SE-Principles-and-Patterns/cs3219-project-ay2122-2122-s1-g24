83c4d599461f216eb877223ee5048e56968fd26c
rishabhpaliwal <rishabh92k@gmail.com>
2021-11-01 17:13:32 +0800

    Fix partnerName

--- frontend/src/component/partnerInfo.js
+++ frontend/src/component/partnerInfo.js
@@ -3,7 +3,7 @@ import { useAuth } from  "../context/AuthContext";
 
 const PartnerInfo = (props) => {
     const { user } =  useAuth();
-    const partnerName;
+    let partnerName = "";
     for(const currUser of props.user) {
         if(currUser.valueOf() != user) {
             partnerName = currUser.valueOf();
@@ -11,7 +11,7 @@ const PartnerInfo = (props) => {
     }
     return (
         <div>
-            <h3>You are playing with: {partnerName === undefined ? "Yourself" : partnerName}</h3>
+            <h3>You are playing with: {partnerName == "" ? "Yourself" : partnerName}</h3>
         </div>
     )
 }
