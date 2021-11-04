5cd9edcccea146062983a26ab1b90672c4eb62c1
Roger <E0406757@u.nus.edu>
2021-11-04 22:29:04 +0800

    Add padding to error pages

--- frontend/src/error/404Page.js
+++ frontend/src/error/404Page.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container'
 const PageNotFound = () => {
     var link = <a href="/">home</a>;
     return (
-        <Container>
+        <Container style={{marginTop: "20px"}}>
             <div>
                 <img src={logo} width="200" height="200" className="center"alt="Peer Prep" />
                 <h2>This page could not be found</h2>
--- frontend/src/error/unAuthorised.js
+++ frontend/src/error/unAuthorised.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container'
 const unathorised = () => {
     var link = <a href="http://localhost:8080/login">Goggle</a>;
     return (
-        <Container>
+        <Container style={{marginTop: "20px"}}>
             <div>
                 <img src={logo} width="200" height="200" className="center"alt="Peer Prep" />
                 <h2>Sorry you need to sign in first</h2>
