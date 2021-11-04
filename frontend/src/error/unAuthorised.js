69a18d602e6c30468ef8bfcaa0c538c6e3a613a0
Roger <E0406757@u.nus.edu>
2021-11-05 00:51:42 +0800

    no message

--- frontend/src/error/unAuthorised.js
+++ frontend/src/error/unAuthorised.js
@@ -2,13 +2,13 @@ import React from 'react'
 import logo from '../assets/logo2.png'
 import Container from 'react-bootstrap/Container'
 const unathorised = () => {
-    var link = <a href="http://localhost:8080/login">Goggle</a>;
+    var link = <a href="http://localhost:8080/login">Google</a>;
     return (
         <Container style={{marginTop: "20px"}}>
             <div>
                 <img src={logo} width="200" height="200" className="center"alt="Peer Prep" />
-                <h2>Sorry you need to sign in first</h2>
-                <p>Click here sign in with {link}</p>
+                <h2>Please sign in to access this page</h2>
+                <p>Click here to sign in with {link}</p>
             </div>
         </Container>
     )
