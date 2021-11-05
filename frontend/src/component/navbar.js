f46c9ebffbe62e6682c74cc76ce2524db1b5d4c3
joshualiangxy <joshualiang.xy@gmail.com>
2021-11-05 14:44:54 +0800

    Add name to navbar

--- frontend/src/component/navbar.js
+++ frontend/src/component/navbar.js
@@ -10,26 +10,34 @@ import logo from '../assets/logo.png'
 const Header = ({ history }) => {
   const { logout: removeAuthInformation } = useAuth();
   const loggedIn = Cookies.get('isLoggedIn');
+  const username = Cookies.get('username');
+  const isAuthenticated = loggedIn === 'true';
 
   const logout = () => {
     removeAuthInformation();
     history.push('/');
   };
 
-  const authButton = loggedIn === 'true' ? ( 
-    <Navbar.Brand onClick={logout}>logout</Navbar.Brand>
+  const authButton = isAuthenticated ? ( 
+    <Navbar.Brand style={{ cursor: 'pointer' }} onClick={logout}>logout</Navbar.Brand>
   ) : (
     <Navbar.Brand href="http://localhost:8080/login">login</Navbar.Brand>
+  );
+  const profilePage = isAuthenticated && (
+    <Navbar.Brand href="/profile">{username}</Navbar.Brand>
   );
 
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/"><img src={logo} width="50" height="50" className="d-inline-block align-top" alt="Peer Prep" /></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src={logo} width="50" height="50" className="d-inline-block align-top" alt="Peer Prep" />
+        </Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
+        <div>
+          {profilePage}
           {authButton}
-        </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
