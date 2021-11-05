2d745625f7bba2bb025ff698d6348797d9b6baac
jazerler <lerjazer@gmail.com>
2021-11-05 23:02:39 +0800

    Fix double sending

--- frontend/src/component/room/codeEnv.js
+++ frontend/src/component/room/codeEnv.js
@@ -56,6 +56,7 @@ const CodeEnv = ({ id, socket, history }) => {
   };
 
   const postAnswers = async (ans) => {
+    if (ans == DEFAULT_VALUE) return;
     const axiosInstance = axios.create({ baseURL: 'http://localhost:8080' });
 
     try {
